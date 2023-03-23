c14ddf33032af63b4d33e9954d91038c15c6866c
ankitaniket <ankitaniket3@gmail.com>
2023-03-23 16:38:47 +0530

    Express.js Server with database

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const db = require('./db.json');
+const app = express();
+
+app.use(cors());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/locations', (req, res) => {
+  res.json(db.employee.map(({ name, lat, lng }) => ({
+    type: 'Feature',
+    properties: { name },
+    geometry: { type: 'Point', coordinates: [lng, lat] }
+  })));
+});
+
+// Route to index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+
+
+
